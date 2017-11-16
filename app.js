33dfc233eed9d9d6eaa9f531c1885ba5b5b64b6b
Matthew Brown <mattd429@gmail.com>
2017-11-16 18:58:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var index = require('./routes/users');
+
+var app = express();
 
