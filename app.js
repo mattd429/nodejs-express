84946e5a2d3c259e40de6ff4025d454dcbbfd59e
Matthew Brown <mattd429@gmail.com>
2017-11-16 19:43:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -38,6 +38,12 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-	//set locals, only providing error in developmen
-})
+	//set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+	
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
 
