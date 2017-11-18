34f13b3906916c6578674b8404f0b59792727d8c
Matthew Brown <mattd429@gmail.com>
2017-11-17 21:55:28 -0500

    Update genre.js

--- models/genre.js
+++ models/genre.js
@@ -1,1 +1,19 @@
+var mongoose = require('mongoose');
 
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema({
+  name: {type: String, required: true, min: 3, max: 100},
+  album: {type: Schema.ObjectId, ref: 'Album', required: true},
+  genre: {type: String, required: true, enum ['electropop', 'hip-hop', 'rock', 'pop', 'dream-pop', 'alternative']},
+});
+
+// virtual for genre
+GenreSchema
+.virtual('url')
+.get(function () {
+  return '/popular/genre/' + this._id;
+});
+
+//Export
+module.exports = mongoose.model('Genre', GenreSchema);
