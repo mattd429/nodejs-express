f1424149736f183f151e9075e93d889dc5399d79
Matthew Brown <mattd429@gmail.com>
2017-11-17 21:48:58 -0500

    Update album.js
    model for Album import mongoose(DB), define Schema for DB(mongoose) created Schema, set URL for album to point to. Export model.

--- models/album.js
+++ models/album.js
@@ -1,1 +1,22 @@
+var mongoose = require('mongoose');
 
+var Schema = mongoose.Schema;
+
+
+var AlbumSchema = new Schema({
+  title: {type: String, required: true},
+  artist: {type: Schema.ObjectId, ref: 'Artist', required: true},
+  summary: {type: String, required: true},
+  isni: {type: Number, required: true, max: 16},
+  genre: [{type: Schema.ObjectId, ref: 'Genre'}] //Not created yet
+});
+
+// Virtual for album's URL
+AlbumSchema
+.virtual('url')
+.get(function () {
+  return '/popular/album/' + this._id;
+});
+
+//Export
+module.exports = mongoose.model('Album', AlbumSchema);
