532198b1d05c4df313aeced65da54945cadeeff3
Matthew Brown <mattd429@gmail.com>
2017-11-18 18:08:34 -0500

    Update genre.js
    album is not required.

--- models/genre.js
+++ models/genre.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 
 var GenreSchema = new Schema({
   name: {type: String, required: true, min: 3, max: 100},
-  album: {type: Schema.ObjectId, ref: 'Album', required: true},
+  album: {type: Schema.ObjectId, ref: 'Album'},
   genre: {type: String, required: true, enum ['electropop', 'hip-hop', 'rock', 'pop', 'dream-pop', 'alternative']},
 });
 
