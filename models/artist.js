662e7075098421815b4b1e26c3c42714f9e24234
Matthew Brown <mattd429@gmail.com>
2017-11-17 21:36:49 -0500

    Update artist.js
    creating a model for our artist, they will have a first and last name with a stage_name(optional) as well as DOB & DOD(optional).  set up a virtual for first and last name and url.

--- models/artist.js
+++ models/artist.js
@@ -1,1 +1,35 @@
+// import mongoose
+var mongoose = required('mongoose');
+
+// create Schema
+var Schema = mongoose.Schema;
+
+// define Schema
+var ArtistSchema = new Schema(
+  {
+    first_name: {type: String, required: true, max: 100},
+    last_name: {type: String, required: true, max: 100},
+    stage_name: {type: String, max: 100},
+    date_of_birth: {type: Date},
+    date_of_birth: {type: Date},
+  }
+);
+
+// Virtual for artist's full name
+ArtistSchema
+.virtual('name')
+.get(function () {
+  return this.last_name + ', ' + this.first_name;
+});
+
+// Virtual for artist's url
+ArtistSchema
+.virtual('url')
+.get(function () {
+  return '/popular/artist/' + this._id;
+});
+
+//export model
+module.exports = mongoose.model('Artist', ArtistSchema);
+
 
