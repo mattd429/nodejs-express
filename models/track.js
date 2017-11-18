409529f3cc546153f65d418c188dee490c6be113
Matthew Brown <mattd429@gmail.com>
2017-11-17 22:11:39 -0500

    Update track.js
    for the track...

--- models/track.js
+++ models/track.js
@@ -1,1 +1,19 @@
+var mongoose = require('mongoose');
 
+var Schema = mongoose.Schema;
+
+var Track = new Schema({
+  name: = { type: String, required: true},
+  album: {type: Schema.ObjectId, ref: 'Album', required: true},
+  track_number: {type: Number, max: 3},
+});
+
+// virtual for track's url
+TrackSchema
+.virtual('url')
+.get(function () {
+  return '/popular/track' + this._id;
+});
+
+//export
+module.exports = mongoose.model('Track', TrackSchema);
