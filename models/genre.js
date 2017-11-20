92a8a4ccf3325044827c0d1fce754db1e89c2c3b
Matthew Brown <mattd429@gmail.com>
2017-11-20 14:39:31 -0500

    Update genre.js
    removed 2 of the Schema entries.

--- models/genre.js
+++ models/genre.js
@@ -4,8 +4,6 @@ var Schema = mongoose.Schema;
 
 var GenreSchema = new Schema({
   name: {type: String, required: true, min: 3, max: 100},
-  album: {type: Schema.ObjectId, ref: 'Album'},
-  genre: {type: String, required: true, enum:['electropop', 'hip-hop', 'rock', 'pop', 'dream-pop', 'alternative']},
 });
 
 // virtual for genre
