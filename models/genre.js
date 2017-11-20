5bfe42ebb6803169bf8b86fd2491b49c0179d58d
Matthew Brown <mattd429@gmail.com>
2017-11-19 19:03:23 -0500

    Update genre.js

--- models/genre.js
+++ models/genre.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 var GenreSchema = new Schema({
   name: {type: String, required: true, min: 3, max: 100},
   album: {type: Schema.ObjectId, ref: 'Album'},
-  genre: {type: String, required: true, enum ['electropop', 'hip-hop', 'rock', 'pop', 'dream-pop', 'alternative']},
+  genre: {type: String, required: true, enum:['electropop', 'hip-hop', 'rock', 'pop', 'dream-pop', 'alternative']},
 });
 
 // virtual for genre
