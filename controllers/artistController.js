c079145c32eaf3040616c654542feeef10dd0fce
Matthew Brown <mattd429@gmail.com>
2017-11-23 10:50:10 -0500

    Update artistController.js

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -10,4 +10,12 @@ exports.artist_detail = function(req, res) {
     res.send('NOT IMPLEMENTED: Artist detail ' +  req.params.id);
 };
 
-//
+// Display Artist create form on GET
+exports.artist_detail_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Artist detail GET');
+};
+
+// Handle Artist create on POST
+exports.artist_detail_post = function(req, res) {
+
+};
