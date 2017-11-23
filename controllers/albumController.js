1cc71c243575ce2d4d4e3b54dc428e15965829f4
Matthew Brown <mattd429@gmail.com>
2017-11-23 15:30:28 -0500

    Update albumController.js

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -1,1 +1,10 @@
+var Album = require('../models/album');
 
+exports.index = function(req, res) {
+    res.send('NOT IMPLEMENTED: Site Home Page');
+};
+
+// Display list of all albums
+exports.album_list = function(req, res) {
+    res.send('NOT IMPLEMENTED: album list');
+};
