3ae5208729d07955860ffb82b8d9d670d57e7a11
Matthew Brown <mattd429@gmail.com>
2017-11-24 18:13:01 -0500

    Update genreController.js

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,1 +1,8 @@
+var Genre = require('../models/genre');
 
+// Display list all of all Genres
+exports.genre_list = function(req, res) {
+    res.send('NOT IMPLEMENTED: Genre list');
+};
+
+// Display detail page for a specific Genre
