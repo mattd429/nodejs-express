c605ddd9dcc24a7fe67d0b34fe06f3f8075489ff
Matthew Brown <mattd429@gmail.com>
2017-11-24 18:39:46 -0500

    Update trackController.js

--- controllers/trackController.js
+++ controllers/trackController.js
@@ -1,1 +1,7 @@
+// note '../' since we are not in the project root
+var Track = require('../models/track');
 
+// Display list of all Tracks
+exports.tracks_list = function(req, res) {
+    res.send('NOT IMPLEMENTED: Track list');
+};
