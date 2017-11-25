229ea32a8654617fead2ce636f2cb0fccd246df7
Matthew Brown <mattd429@gmail.com>
2017-11-25 17:28:55 -0500

    Update inventory.js

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,1 +1,10 @@
+var express = require('express');
+var router = express.Router();
 
+// Require our controllers
+var album_controller = require('../controllers/albumController');
+var artist_controller = require('../controllers/artistController');
+var genre_controller = require('../controllers/genreController');
+var track_controller = require('../controllers/trackController');
+
+/// ALBUM ROUTES ///
