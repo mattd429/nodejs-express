a31e805b80cf854a668804b6d4f44df5b42b66cc
Matthew Brown <mattd429@gmail.com>
2017-11-25 14:31:47 -0500

    Update trackController.js

--- controllers/trackController.js
+++ controllers/trackController.js
@@ -6,9 +6,24 @@ exports.tracks_list = function(req, res) {
     res.send('NOT IMPLEMENTED: Track list');
 };
 
-// Display detail page for a specific Track
+// Display track detail page for a specific Track
 exports.track_detail = function(req, res) {
     res.send('NOT IMPLEMENTED: Track detail: ' + req.params.id);
+};
+
+// Display track create form on GET
+exports.track_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Track create GET');
+};
+
+// Handle Track create on POST
+exports.track_create_post = function(req, res) {
+    res.send('NOT IMPLMENTED: Track create POST');
+};
+
+// Display Track delete form on GET
+exports.track_delete_get = function(req, res) {
+    res.send('')  
 };
 
 
