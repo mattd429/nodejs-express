8a7c7c22b96e3f51ac0c02963e89feda753cc137
Matthew Brown <mattd429@gmail.com>
2017-11-26 16:42:56 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,9 @@
+var express = require('express');
+var router = express.Router();
 
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.redirect('/inventory'); // redirect our index to inventory.js 
+});
+
+module.exports = router;
