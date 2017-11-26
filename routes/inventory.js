4b6ce5bf3edad11f5b6880d6e404de2fff37e256
Matthew Brown <mattd429@gmail.com>
2017-11-26 15:39:55 -0500

    Update inventory.js

--- routes/inventory.js
+++ routes/inventory.js
@@ -44,3 +44,6 @@ router.get('/artist/create', artist_controller.artist_create_get);
 /* POST request for creating Artist. */
 router.post('/artist/create', artist_controller.artist_create_post);
 
+/* GET request for deleting Artist. */
+router.get('/artist/delete', artist_controller.artist_delete_get);
+
