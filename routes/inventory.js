258152ce04d2698457329a7a715203c096957f56
Matthew Brown <mattd429@gmail.com>
2017-11-25 21:39:04 -0500

    Update inventory.js

--- routes/inventory.js
+++ routes/inventory.js
@@ -29,3 +29,6 @@ router.get('/album/:id/update', album_controller.album_update_get);
 
 /* POST request to update Album. */
 router.post('/album/:id/update', album_controller.album_update_post);
+
+/* GET request for one Album. */
+router.get('/album/:id', album_controller.album_detail);
